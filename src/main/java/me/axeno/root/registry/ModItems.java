c5ee7841172a0d7fc7ee35c17221cd45a1c40fe9
Gexary <gexary.gex@gmail.com>
2026-08-28 21:32:01 +0200

    Fix de texture

--- src/main/java/me/axeno/root/registry/ModItems.java
+++ src/main/java/me/axeno/root/registry/ModItems.java
@@ -6,7 +6,6 @@ import me.axeno.root.registry.utils.RegistryHelper;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,15 +18,6 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Root.MODID);
     public static final RegistryObject<Item> ROOT_HEART = registerItem("root_heart");
     public static final RegistryObject<Item> ROOT = registerItem("root", () -> new RootItem(RegistryHelper.itemProps()));
-    public static final RegistryObject<Item> ROOT_SPAWN_EGG = ITEMS.register(
-            "root_spawn_egg",
-            () -> new ForgeSpawnEggItem(
-                    ModEntityTypes.ROOT_ENTITY,
-                    0xffffff,
-                    0xffffff,
-                    new Item.Properties()
-            )
-    );
 
     private ModItems() {
     }
